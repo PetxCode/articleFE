e166870ff5555fc2f4f891925833cf21cec23747
Peter Oti <71887594+PetxCode@users.noreply.github.com>
2023-07-31 19:30:14 +0100

    stop here

---
+++ src/api/ArticleAPI.tsx
@@ -0,0 +1,18 @@
+import axios from "axios"
+
+const url: string = 'http://localhost:3344/api/v1'
+
+export const createArticle = async (data: any, userID: string) => {
+    try {
+        const config: {} = {
+            'content-type': "multipart/form-data"
+        }
+
+        return await axios.post(`${url}/${userID}/create-article`, data, config).then((res: any) => {
+            return res.data.data
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+} 
---
+++ src/hooks/useAuthor.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import { viewAuthor } from '../api/AuthorAPI'
+
+import { useQuery } from "@tanstack/react-query"
+
+export const useAuthor = () => {
+
+    const [authors, setAuthors] = useState([])
+
+    useEffect(() => {
+        viewAuthor().then((res) => {
+            setAuthors(res)
+        })
+    }, [])
+
+    return authors
+}
+
+
+export const useTanAuthor = () => {
+    const { data: authors, error, isLoading } = useQuery({
+        queryKey: ['viewAuthor'],
+        queryFn: viewAuthor
+    })
+
+
+    return { authors, error, isLoading }
+}
+
